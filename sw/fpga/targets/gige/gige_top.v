03bbd9bc8a293d7cdb714dbc41d62ebb67989c6a
Dominic Spill <dominicgs@gmail.com>
2013-05-02 07:47:23 +1000

    Correct typos in GigE pin names

--- sw/fpga/targets/gige/gige_top.v
+++ sw/fpga/targets/gige/gige_top.v
@@ -25,15 +25,76 @@
 
 module gige_top (
 	
-input	wire			clk_50,
+input   wire          clk_50,
 
 // Hex LEDs
-output	wire	[6:0]	HEX0,
-output	wire	[6:0]	HEX1,
-output	wire	[6:0]	HEX2,
-	
-input	wire	[3:0]	dbg_button,
-output	reg		[17:0]	dbg_led
+output  wire  [6:0]   HEX0,
+output  wire  [6:0]   HEX1,
+output  wire  [6:0]   HEX2,
+output  wire  [6:0]   HEX3,
+output  wire  [6:0]   HEX4,
+output  wire  [6:0]   HEX5,
+output  wire  [6:0]   HEX6,
+output  wire  [6:0]   HEX7,
+
+input	wire  [3:0]   KEY,
+input	wire  [17:0]  SW,
+output	wire  [17:0]  LEDR,
+output  wire  [8:0]   LEDG,
+
+// Ethernet PHY 0
+output  wire  [7:0]   phy0_gm_txd,
+output  wire          phy0_gm_tx_en,
+output  wire          phy0_gm_tx_er,
+output  wire          phy0_gm_tx_clk,
+output  wire          phy0_gm_gtx_clk,
+
+// Inputs (outputs in configuration mode)
+inout   wire  [7:0]   phy0_gm_rxd,
+inout   wire          phy0_gm_rx_dv,
+
+input   wire          phy0_gm_rx_err,
+input   wire          phy0_gm_rx_clk,
+
+// Have internal pull-up (not sure how that affects the FPGA)
+output  wire          phy0_gm_mdc,
+inout   wire          phy0_gm_mio,
+output  wire          phy0_hw_rst,
+
+// Used for configuration mode (LED states in all other modes)
+inout   wire  [0:4]   phy0_addr,
+
+input   wire          phy0_int_n,
+input   wire          phy0_clk125_ndo,
+input   wire          phy0_col,
+input   wire          phy0_crs,
+
+// Ethernet PHY 1
+output  wire  [7:0]   phy1_gm_txd,
+output  wire          phy1_gm_tx_en,
+output  wire          phy1_gm_tx_er,
+output  wire          phy1_gm_tx_clk,
+output  wire          phy1_gm_gtx_clk,
+
+// Inputs (outputs in configuration mode)
+inout   wire  [7:0]   phy1_gm_rxd,
+inout   wire          phy1_gm_rx_dv,
+
+input   wire          phy1_gm_rx_err,
+input   wire          phy1_gm_rx_clk,
+
+// Have internal pull-up (not sure how that affects the FPGA)
+output  wire          phy1_gm_mdc,
+inout   wire          phy1_gm_mio,
+output  wire          phy1_hw_rst,
+
+// Used for configuration mode (LED states in all other modes)
+inout   wire  [0:4]   phy1_addr,
+
+input   wire          phy1_int_n,
+input   wire          phy1_clk125_ndo,
+input   wire          phy1_col,
+input   wire          phy1_crs
 );
 
 
