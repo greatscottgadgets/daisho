95b56c0d961b0fec9b92db007ae9c8c672e454fc
Felipe Balbi <balbi@kernel.org>
2015-03-10 23:20:44 -0500

    daisho_extcap: miscellaneous cleanups
    return is not a function, so remove () around its
    return value, return negative value on error, indentation.
    
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- sw/host/daisho_extcap.c
+++ sw/host/daisho_extcap.c
@@ -4,7 +4,8 @@
 #include <string.h>
 #include <unistd.h>
 
-void list_interfaces() {
+void list_interfaces()
+{
 	/* Adapt to count attached Daisho devices in future
 	 * Identify them by bus/device number or serial?
 	 */
@@ -13,17 +14,19 @@ void list_interfaces() {
 	printf("interface {display=Daisho2}{value=/dev/ttyUSB2}\n");
 }
 
-void list_dlts() {
+void list_dlts()
+{
 	printf("dlt {number=1}{name=EN10MB}{display=Ethernet (Daisho)}\n");
 }
 
-void show_config() {
+void show_config()
+{
 	return;
 	printf("arg {number=0}{call=device}{display=Serial Device}{type=selector}\n");
-
 }
 
-void spawn_daisho_en_uart(char *source, char *fifo) {
+void spawn_daisho_en_uart(char *source, char *fifo)
+{
 	fprintf(stderr, "daisho-en-uart -s %s -p %s\n", source, fifo);
 	execlp("daisho-en-uart", "daisho-en-uart", "-s", source, "-p", fifo, NULL);
 }
@@ -37,7 +40,8 @@ typedef enum {
 	FIFO
 } options;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	// Turn off the getopt error reporting
 	opterr = 0;
 	optind = 0;
@@ -58,61 +62,61 @@ int main(int argc, char *argv[]) {
 
 	while ((arg = getopt_long(argc, argv, "", longopts, &option_idx)) >= 0) {
 		switch (arg) {
-			case LIST_INTERFACES:
-				list_interfaces();
-				return 0;
-			case CONFIG:
-				do_config = 1;
-				break;
-			case INTERFACE:
-				interface = strdup(optarg);
-				break;
-			case LIST_DLTS:
-				do_dlts = 1;
-				break;
-			case CAPTURE:
-				do_daisho_en_uart = 1;
-				break;
-			case FIFO:
-				fifo = strdup(optarg);
-				break;
+		case LIST_INTERFACES:
+			list_interfaces();
+			return 0;
+		case CONFIG:
+			do_config = 1;
+			break;
+		case INTERFACE:
+			interface = strdup(optarg);
+			break;
+		case LIST_DLTS:
+			do_dlts = 1;
+			break;
+		case CAPTURE:
+			do_daisho_en_uart = 1;
+			break;
+		case FIFO:
+			fifo = strdup(optarg);
+			break;
 		}
 	}
 
 	if (do_daisho_en_uart) {
 		if (fifo == NULL)
-			return(1);
+			return -1;
 
 		spawn_daisho_en_uart(interface, fifo);
 	}
 
 	if (do_config) {
 		if (interface == NULL)
-			return(1);
+			return -1;
 
 		if (strcmp(interface, "/dev/ttyUSB0") != 0 &&
 			strcmp(interface, "/dev/ttyUSB1") != 0 &&
 			strcmp(interface, "/dev/ttyUSB2") != 0)
-			return(1);
+			return -1;
 
 		show_config();
 
-		return(0);
+		return 0;
 	}
 
 	if (do_dlts) {
 		if (interface == NULL)
-			return(1);
+			return -1;
 
 		if (strcmp(interface, "/dev/ttyUSB0") != 0 &&
 			strcmp(interface, "/dev/ttyUSB1") != 0 &&
 			strcmp(interface, "/dev/ttyUSB2") != 0)
-			return(1);
+			return -1;
 
 		list_dlts();
 
-		return(0);
+		return 0;
 	}
 
-	return(1);
+	return -1;
 }
