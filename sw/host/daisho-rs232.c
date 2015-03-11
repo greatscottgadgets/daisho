edc4c0cccfb4683ec32a75f78e8fcae6264e38b0
Felipe Balbi <balbi@kernel.org>
2015-03-10 22:42:48 -0500

    daisho-rs232: miscellaneous cleanup
    indentation, whitespaces, and the like.
    
    Signed-off-by: Felipe Balbi <balbi@kernel.org>

--- sw/host/daisho-rs232.c
+++ sw/host/daisho-rs232.c
@@ -27,7 +27,7 @@
 #define BUF_LEN 512
 int main(void)
 {
-    int i, ret, transferred;
+	int i, ret, transferred;
 	struct libusb_device_handle *dev = NULL;
 	unsigned char readback[BUF_LEN];
 
@@ -45,42 +45,46 @@ int main(void)
 		printf("Failed to find Daisho device\n");
 		return -1;
 	}
+
 	printf("got device\n");
 	ret = libusb_set_configuration(dev, 1);
-    if (ret < 0) {
+	if (ret < 0) {
 		printf("* Can't set config: %s\n", libusb_error_name(ret));
-        libusb_close(dev);
-        return -1;
-    } else {
-        printf("* Set configuration \n");
-    }
+		libusb_close(dev);
+		return -1;
+	} else {
+		printf("* Set configuration \n");
+	}
 
 	ret = libusb_claim_interface(dev, 0);
-    if (ret < 0) {
-        printf("* Can't claim interface: %s\n", libusb_error_name(ret));
-        libusb_close(dev);
-        return -1;
-    } else {
-        printf("* Claimed interface\n");
-    }
+	if (ret < 0) {
+		printf("* Can't claim interface: %s\n", libusb_error_name(ret));
+		libusb_close(dev);
+		return -1;
+	} else {
+		printf("* Claimed interface\n");
+	}
 
 	while(1) {
-		ret = libusb_bulk_transfer(dev, 0x81, readback, BUF_LEN, &transferred, 0);
+		ret = libusb_bulk_transfer(dev, 0x81, readback, BUF_LEN,
+				&transferred, 0);
 		if (ret < 0){
 			printf("* Couldn't read: %s\n", libusb_error_name(ret));
 			break;
-		/* Ignore case where we have an incomplete state */
+			/* Ignore case where we have an incomplete state */
 		} else if (transferred == 8) {
-			for(i=0;i<transferred;i++)
+			for (i = 0; i < transferred; i++)
 				printf("%02x ", readback[i]);
 			printf("\n");
 		}
 		fflush(NULL);
 	}
 
-    libusb_release_interface(dev, 0);
-	if(dev) libusb_close(dev);
+	libusb_release_interface(dev, 0);
+	if(dev)
+		libusb_close(dev);
 
-    printf("\n* Finished\n");
-    return 0;
+	printf("\n* Finished\n");
+
+	return 0;
 }
